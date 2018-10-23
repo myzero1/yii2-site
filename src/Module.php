5b5baa778e525feca5f3109bf51cc0b36b1e7109
qinxuanwu <qinxuanwu@yungengxin.com>
2018-10-23 12:22:58 +0800

    holdon

--- src/Module.php
+++ src/Module.php
@@ -10,14 +10,31 @@ use Yii;
 class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
 {
     /**
+     * @inheritdoc
+     */
+    public $theme = 'adminlteiframe'; // adminlteiframe,adminlte
+
+    /**
      * {@inheritdoc}
      */
     public function bootstrap($app)
     {
         if ($app instanceof \yii\web\Application) {
-            $app->controllerMap['site'] = [
+          /*  $app->controllerMap['site'] = [
                 'class'=>'myzero1\site\controllers\SiteController'
-            ];
+            ];*/
+            $app->getUrlManager()->addRules([
+                [
+                    'class' => 'yii\web\UrlRule',
+                    'pattern' => '',
+                    'route' => $this->id . '/site/index'
+                ],
+                [
+                    'class' => 'yii\web\UrlRule',
+                    'pattern' => 'site',
+                    'route' => $this->id . '/site/index'
+                ],
+            ], false);
         } elseif ($app instanceof \yii\console\Application) {
             //
         }
